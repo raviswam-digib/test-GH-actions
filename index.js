bf2512b980115ab855bbcc344fa919ec5f419e93
raviswamjpmc <110006831+raviswamjpmc@users.noreply.github.com>
2022-10-17 23:24:46 +0800

    My first action is ready

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ try {
 } catch (error) {
   core.setFailed(error.message);
 }
+try {
+  // `who-to-greet` input defined in action metadata file
+  const email = core.getInput('email');
+  console.log(`Hello ${email}!`);
+  const emailmsg = core.getInput('emailmsg');
+  console.log(`Hello ${emailmsg}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
