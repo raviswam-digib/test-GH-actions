9d013dae15a6bfc71cd02dd198254f222eedea09
raviswamjpmc <110006831+raviswamjpmc@users.noreply.github.com>
2022-10-18 12:15:20 +0800

    My first action is ready

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 git add .
 git commit -m 'My first action is ready'
-git tag -a -m 'My first action release' 'v14.1'
+git tag -a -m 'My first action release' 'v15.1'
 git push --follow-tags
